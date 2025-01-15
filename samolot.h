3a2e8ee27754bbf2d9d9fab65fae9b7d87232cad
NKKOW <163651824+NKKOW@users.noreply.github.com>
2025-01-16 00:19:08 +0100

    Update samolot.h
    Poprawa kodu, zmiana miejsca zmiennych (dalej prototyp pod rozszerzenie)

--- samolot.h
+++ samolot.h
@@ -1,10 +1,13 @@
 #ifndef SAMOLOT_H
 #define SAMOLOT_H
 
-void* samolot_func(void* arg);
+#include <pthread.h>
 
+//Deklaracja zmiennych globalnych extern z pliku głównego symulacji
 extern pthread_mutex_t mutex; 
 extern int capacity; // pojemność samolotu obecnie  
 extern int P;  // pojemność samolotu ogólnie
+
+void* samolot_func(void* arg);
 
 #endif
