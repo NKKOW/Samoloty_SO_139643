6fa19a06d4a716c0db62589a05e66124c2480407
NKKOW <163651824+NKKOW@users.noreply.github.com>
2025-01-17 00:13:00 +0100

    test

--- pasazer.c
+++ pasazer.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
-#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "pasazer.h"
 
-extern pthread_mutex_t mutex;
-extern int capacity; // liczba wolnych miejsc
+int bagaz_waga()
+{
+    return (rand() % 13) + 1;
+}
 
 void* pasazer_func(void* arg){
     long id = (long) arg;
 
+    //Odprawa bagażowa
+    int pasazer_bagaz = bagaz_waga();
+    sem_wait(&bagaz_wagaSem);
+    int bagaz_checktime = rand() % 3;
+    printf("Pasazer %ld rozpoczyna kontole bagazu. Waga bagazu %d.\n", id, pasazer_bagaz);
+    sleep(bagaz_checktime);
+
+    if (pasazer_bagaz > M)
+    {
+        printf("Pasazer %ld odrzucony. Waga bagazu %d większa od dopuszczalnego %d. Odprawa do terminala.\n", id, pasazer_bagaz, M);
+        //zwolnienie stanowiska
+        sem_post(&bagaz_wagaSem);
+
+        pthread_mutex_lock(&mutex);
+        licznik_pasazer++;
+        pthread_mutex_unlock(&mutex);
+        
+        return NULL;
+    }
+    else 
+    {
+        printf("Pasażer %ld zakończył kontrolę bagażu (OK). Waga=%d\n", id, pasazer_bagaz);
+        sem_post(&bagaz_wagaSem);
+    }
+
+    //Pętla sprawdzająca liczbę wolnych miejsc
     pthread_mutex_lock(&mutex);
     if (capacity > 0 )
     {
@@ -16,7 +45,7 @@ void* pasazer_func(void* arg){
     }
     else
     {
-        printf("Pasazer %ld nie wsiadl. Samolot zapelniony", id);
+        printf("Pasazer %ld nie wsiadl. Samolot zapelniony\n", id);
     }
     pthread_mutex_unlock(&mutex);
     return NULL;
